01e3703e1453d8df7e2eef3569b685f2fdacd24f
derysudrajat <dery.sudrajat17@gmail.com>
2019-07-06 15:10:30 +0700

    add collision event handle, add mouse x input handle

--- Assets/AurynSky/ArenaPacks/Forest Pack/Scripts/Kapsul.cs
+++ Assets/AurynSky/ArenaPacks/Forest Pack/Scripts/Kapsul.cs
@@ -5,12 +5,43 @@ using UnityEngine;
 public class Kapsul : MonoBehaviour
 {
     // Start is called before the first frame update
+    public bool isGrounded;
+    public bool isLadder;
     public int moveSpeed;
     public int rotationSpeed;
-    
+    public int jumpHeight;
+    public float jumpSpeed;
+    Rigidbody rb;    
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == ("Ground") && isGrounded == false)
+        {
+            isGrounded = true;
+            print("Object: Ground");
+        }
+        if (col.gameObject.tag == "Ladder")
+        {
+            isLadder = true;
+            print("Object: Ladder");
+        }
+        if (col.gameObject.tag == "DoorStart")
+        {
+            print("Object: DoorStart");
+        }
+        if (col.gameObject.tag == "DoorFinish")
+        {
+            print("Object: DoorFinish");
+        }
+        if (col.gameObject.tag == "WinFlag")
+        {
+            print("Object: WinFlag");
+        }
     }
 
     // Update is called once per frame
@@ -20,5 +51,29 @@ public class Kapsul : MonoBehaviour
         Vector3 rotation = transform.eulerAngles;
         rotation.y += Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime; // Standart Left-/Right Arrows and A & D Keys
         transform.eulerAngles = rotation;
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || isLadder))
+        {
+            rb.AddForce(new Vector3(0, jumpHeight, 0) * jumpSpeed, ForceMode.Impulse);
+            isGrounded = false;
+            if (isLadder)
+            {
+                isLadder = false;
+            }
+        }
+        if (Input.GetAxis("Mouse X") < 0)
+        {
+            //Code for action on mouse moving left
+            print("Mouse moved left");
+        }
+        if (Input.GetAxis("Mouse X") > 0)
+        {
+            //Code for action on mouse moving right
+            print("Mouse moved right");
+        }
+        if (gameObject.tag == "Coin")
+        {
+            print("Coin uuuuuu");
+        }
+        
     }
 }
