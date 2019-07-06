6f339d5bc9ad7fd120f8210cc4fdd88608a303ad
derysudrajat <dery.sudrajat17@gmail.com>
2019-07-06 12:57:10 +0700

    Initial Unity Project

---
+++ Assets/AurynSky/ArenaPacks/Forest Pack/Scripts/Kapsul.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kapsul : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int moveSpeed;
+    public int rotationSpeed;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        Vector3 rotation = transform.eulerAngles;
+        rotation.y += Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime; // Standart Left-/Right Arrows and A & D Keys
+        transform.eulerAngles = rotation;
+    }
+}
