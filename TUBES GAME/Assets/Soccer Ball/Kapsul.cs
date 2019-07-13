cf8197cfb96b40cde8d3b1d4e5ec7a2f6eef89e4
kukuhpr <primaditokukuh@gmail.com>
2019-07-13 17:45:33 +0700

    menambahkan scene 3

---
+++ TUBES GAME/Assets/Soccer Ball/Kapsul.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kapsul : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isGrounded;
+    public bool isLadder;
+    public int moveSpeed;
+    public int rotationSpeed;
+    public int jumpHeight;
+    public float jumpSpeed;
+    Rigidbody rb;    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == ("Ground") && isGrounded == false)
+        {
+            isGrounded = true;
+            print("Object: Ground");
+        }
+        if (col.gameObject.tag == "Ladder")
+        {
+            isLadder = true;
+            print("Object: Ladder");
+        }
+        if (col.gameObject.tag == "DoorStart")
+        {
+            print("Object: DoorStart");
+        }
+        if (col.gameObject.tag == "DoorFinish")
+        {
+            print("Object: DoorFinish");
+        }
+        if (col.gameObject.tag == "WinFlag")
+        {
+            print("Object: WinFlag");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        Vector3 rotation = transform.eulerAngles;
+        rotation.y += Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime; // Standart Left-/Right Arrows and A & D Keys
+        transform.eulerAngles = rotation;
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || isLadder))
+        {
+            rb.AddForce(new Vector3(0, jumpHeight, 0) * jumpSpeed, ForceMode.Impulse);
+            isGrounded = false;
+            if (isLadder)
+            {
+                isLadder = false;
+            }
+        }
+        if (Input.GetAxis("Mouse X") < 0)
+        {
+            //Code for action on mouse moving left
+            print("Mouse moved left");
+        }
+        if (Input.GetAxis("Mouse X") > 0)
+        {
+            //Code for action on mouse moving right
+            print("Mouse moved right");
+        }
+        if (gameObject.tag == "Coin")
+        {
+            print("Coin uuuuuu");
+        }
+        
+    }
+}
